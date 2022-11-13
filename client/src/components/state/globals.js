5f810df788591cf39e3061ade8e18a834d9e6ea5
aphemra <aphemra@gmail.com>
2022-11-13 02:09:24 -0500

    Added temporary game loop to Combat.js * Uses UseEffect and setInterval * Tick speed of game loop controlled by global tickspeed atom.

--- client/src/components/state/globals.js
+++ client/src/components/state/globals.js
@@ -2,6 +2,7 @@ import { atom } from "jotai";
 import { setLevels } from "../../utils/levelUtils";
 
 export const levels = setLevels(100);
+export const tickSpeed = atom(1000);
 
 const inventory = atom([]);
 export const skills = atom([
@@ -10,7 +11,7 @@ export const skills = atom([
 	{ name: "health", currentExp: 0, currentLevel: 1 },
 	{ name: "defense", currentExp: 0, currentLevel: 1 },
 ]);
-const money = atom(0);
+export const money = atom(0);
 
 export function getLevelFromExperience(experience) {
 	return levels.filter((level) => experience >= level.exp).length;
--- client/src/pages/Combat.js
+++ client/src/pages/Combat.js
@@ -1,13 +1,33 @@
 import { useAtom } from "jotai";
-import { useState } from "react";
-import { skills as sk, getLevelFromExperience as getLevel } from "../components/state/globals";
+import { useEffect, useState } from "react";
+import { skills as sk, getLevelFromExperience as getLevel, tickSpeed as ts, money as m } from "../components/state/globals";
 import s from "../styles/modules/Combat.module.scss";
 
 export default function Combat() {
 	const [skills, setSkills] = useAtom(sk);
-	const [expGained, setExpGained] = useState(1);
+	const [tickSpeed, setTickSpeed] = useAtom(ts);
+	const [money, setMoney] = useAtom(m);
 
-	function handleTrainSkill(skillToTrain, expGained) {
+	const [currentSkill, setCurrentSkill] = useState();
+
+	const expGained = 15;
+
+	// Game loop probably
+	useEffect(() => {
+		const interval = setInterval(() => {
+			// Logic to be done every tickSpeed milliseconds
+			if (!currentSkill) return;
+			trainSkill(currentSkill, expGained);
+		}, tickSpeed);
+
+		return () => clearInterval(interval);
+	});
+
+	function handleSkillSwitch(skillToTrain) {
+		setCurrentSkill(skillToTrain);
+	}
+
+	function trainSkill(skillToTrain, expGained) {
 		setSkills((previousState) => {
 			const newState = previousState.map((skill) => {
 				if (skill.name === skillToTrain) {
@@ -20,18 +40,18 @@ export default function Combat() {
 		});
 	}
 
-	function handleInputChange(e) {
-		setExpGained(parseInt(e.target.value));
-	}
-
 	// Need to eventually move training buttons to their own component so they don't share state
 	return (
 		<div className={s.combat}>
 			Combat
-			<input onChange={handleInputChange} type="number" placeholder="Increase by..." defaultValue={1} />
+			{/* <div>{`Money: ${money}`}</div> */}
+			<div className={s.training}>{`Training: ${currentSkill || "Nothing"}`}</div>
+			<button className={s.stopTraining} type="button" onClick={() => handleSkillSwitch(undefined)}>
+				Stop Training
+			</button>
 			{skills.map((skill) => {
 				return (
-					<button key={skill.name} type="button" onClick={() => handleTrainSkill(skill.name, expGained)}>
+					<button key={skill.name} type="button" onClick={() => handleSkillSwitch(skill.name)}>
 						Train {skill.name}
 					</button>
 				);
